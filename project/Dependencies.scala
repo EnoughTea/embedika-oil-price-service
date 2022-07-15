66285bc37c3783eb0cf55edaa29ecf910899a24b
Konstantin Zakharov <enough.tea@outlook.com>
2022-07-15 15:03:54 +0300

    Dependencies for further web scraping and csv parsing

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,20 @@ import sbt.*
 
 object Dependencies {
   val configVersion         = "1.4.2"
+  val csvParserVersion      = "2.1.0"
   val logbackClassicVersion = "1.2.11"
   val scalaLoggingVersion   = "3.9.5"
+  val scalaScraperVersion   = "3.0.0"
   val scalatestVersion      = "3.2.12"
+  val sttpVersion           = "3.6.2"
 
   val config         = "com.typesafe"                   % "config"          % configVersion
+  val csvParser      = "de.siegmar"                     % "fastcsv"         % csvParserVersion
   val logbackClassic = "ch.qos.logback"                 % "logback-classic" % logbackClassicVersion
   val scalaLogging   = "com.typesafe.scala-logging"    %% "scala-logging"   % scalaLoggingVersion
+  val scalaScraper   = "net.ruippeixotog"              %% "scala-scraper"   % scalaScraperVersion
   val scalatest      = "org.scalatest"                 %% "scalatest"       % scalatestVersion
+  val sttp           = "com.softwaremill.sttp.client3" %% "core"            % sttpVersion
 
   val loggingLibs = Seq(logbackClassic, scalaLogging)
 
@@ -18,5 +24,8 @@ object Dependencies {
 
   val opsDeps = Seq(
     config,
+    csvParser,
+    scalaScraper,
+    sttp
   ) ++ loggingLibs ++ testLibs
 }
