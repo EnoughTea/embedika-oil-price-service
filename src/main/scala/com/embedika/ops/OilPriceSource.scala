385a01663bda3538dd0211b7f7a132b9be9ffcb4
Konstantin Zakharov <enough.tea@outlook.com>
2022-07-15 14:43:19 +0300

    Add simple local oil price source (JAR resource)

--- src/main/scala/com/embedika/ops/OilPriceSource.scala
+++ src/main/scala/com/embedika/ops/OilPriceSource.scala
@@ -4,6 +4,7 @@ import java.io.{InputStream, InputStreamReader}
 
 import scala.concurrent.Future
 
+
 /** Used for something capable of providing something containing oil prices in an unknown format. */
 trait OilPriceSource:
   /** Fetches oil prices from a local source. */
@@ -12,10 +13,12 @@ trait OilPriceSource:
   /** Fetches oil prices from a remote source. */
   def remote()(using ec: IoExecutionContext): Future[InputStreamReader]
 
+
 /** Provides CSV stream readers for data.gov.ru oil prices. */
 final class DataGovRuOilPriceSource() extends OilPriceSource:
+  /** Fetches oil prices from a JAR resource. Not a good idea for a real service, but good enough for our purposes. */
   override def local()(using ec: IoExecutionContext): Future[InputStreamReader] = Future {
-    new InputStreamReader(InputStream.nullInputStream())
+    scala.io.Source.fromResource("data-20220617T1317-structure-20210419T0745.csv").reader()
   }
 
   override def remote()(using ec: IoExecutionContext): Future[InputStreamReader] = Future {
