8b0f5bd7ea286471d8755463ba26d9fd58f407b2
Konstantin Zakharov <enough.tea@outlook.com>
2022-07-16 16:46:02 +0300

    Finished OilPriceService routes

--- src/main/scala/com/embedika/ops/OilPriceServiceRoutes.scala
+++ src/main/scala/com/embedika/ops/OilPriceServiceRoutes.scala
@@ -3,13 +3,14 @@ package com.embedika.ops
 import java.time.LocalDate
 
 import scala.concurrent.Future
-import scala.util.*
+import scala.util.{Failure, Success, Try}
 
 import akka.http.scaladsl.model.*
 import akka.http.scaladsl.server.*
 import akka.http.scaladsl.server.Directives.*
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import io.circe.generic.auto.*
+import squants.market.Money
 
 
 trait OilPriceServiceRoutes extends Routes with FailFastCirceSupport with OilPriceRecordJsonFormat {
@@ -22,12 +23,73 @@ trait OilPriceServiceRoutes extends Routes with FailFastCirceSupport with OilPri
       get {
         concat(
           path("all") {
-            val eventualResponse = service.allRecords(providerId) map (EntityResponse(_))
+            val eventualResponse = service.allRecords(providerId) map (ApiResponse.entity(_))
             ApiResponse.completeWith(eventualResponse)
           },
+          path("stats") {
+            final case class RecordStats(total: Long)
+            val eventualResponse = service.allRecords(providerId) map { records =>
+              EntityResponse(RecordStats(records.length))
+            }
+            ApiResponse.completeWith(eventualResponse)
+          },
+          (path("single") & parameterMap) { params =>
+            val eventualResponse = withLocalDateParam("date", params) { date =>
+              service.priceAtDate(date, providerId) map {
+                case Some(price) => ApiResponse.entity(price)
+                case None        => ApiResponse.notFound(s"No oil price records exist for $date")
+              }
+            }
+            ApiResponse.completeWith(eventualResponse)
+          },
+          (path("range") & parameterMap) { params =>
+            val eventualResponse = withLocalDateParam("startDate", params) { startDate =>
+              withLocalDateParam("endDate", params) { endDate =>
+                val dateRange = DateRange(startDate, endDate)
+                service.priceInDateRange(dateRange, providerId) map {
+                  case Some(price) => ApiResponse.entity(price)
+                  case None =>
+                    ApiResponse.notFound(s"Given date range $dateRange is disjointed with all oil price records")
+                }
+              }
+            }
+            ApiResponse.completeWith(eventualResponse)
+          },
+          (path("minmax") & parameterMap) { params =>
+            val eventualResponse = withLocalDateParam("startDate", params) { startDate =>
+              withLocalDateParam("endDate", params) { endDate =>
+                val dateRange = DateRange(startDate, endDate)
+                service.minMaxPricesInDateRange(dateRange, providerId) map {
+                  case Some(prices) => ApiResponse.entity(prices)
+                  case None =>
+                    ApiResponse.notFound(s"Given date range $dateRange is disjointed with all oil price records")
+                }
+              }
+            }
+            ApiResponse.completeWith(eventualResponse)
+          }
         )
       }
     }
 
+  }
+
+  def withLocalDateParam(paramName: String, params: Map[String, String])(
+      func: LocalDate => Future[ApiResponse]
+  ): Future[ApiResponse] = {
+    val maybeTriedDate = params.get(paramName).map(dateRepr => Try(LocalDate.parse(dateRepr)))
+    maybeTriedDate match {
+      case Some(triedDate) =>
+        triedDate match {
+          case Failure(_) =>
+            Future.successful(
+              ApiResponse.badRequest(
+                s"$paramName does not contain a valid date, should be ISO local date text like \"2007-12-03\""
+              )
+            )
+          case Success(date) => func(date)
+        }
+      case None => Future.successful(ApiResponse.badRequest(s"$paramName is missing from parameters"))
+    }
   }
 }
