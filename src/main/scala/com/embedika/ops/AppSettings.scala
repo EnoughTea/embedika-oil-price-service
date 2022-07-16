e37beba1ae655652aa6aea745ac8eeaf73f1df80
Konstantin Zakharov <enough.tea@outlook.com>
2022-07-16 15:02:06 +0300

    Add routes helper trait

--- src/main/scala/com/embedika/ops/AppSettings.scala
+++ src/main/scala/com/embedika/ops/AppSettings.scala
@@ -11,19 +11,20 @@ import com.typesafe.config.{Config, ConfigFactory}
 final case class DataGovRuSettings(oilPageLink: URI, oilPageCsvLinkQuery: String)
 
 
+final case class AppSettings(
+    config: Config,
+    host: String,
+    port: Int,
+    requestIdHeaderName: String,
+    dataGovRu: DataGovRuSettings
+)
+
+
 object DataGovRuSettings {
   def apply(config: Config): Try[DataGovRuSettings] = Try {
     DataGovRuSettings(new URI(config.getString("oil-page-link")), config.getString("oil-page-csv-link-query"))
   }
 }
-
-
-final case class AppSettings(
-    config: Config,
-    host: String,
-    port: Int,
-    dataGovRu: DataGovRuSettings
-)
 
 
 object AppSettings {
@@ -33,13 +34,8 @@ object AppSettings {
       config,
       appConfig.getString("host"),
       appConfig.getInt("port"),
+      appConfig.getString("request-id-header-name"),
       DataGovRuSettings(appConfig.getConfig("data-gov-ru")).get
     )
   }
-}
-
-
-trait HasSettings {
-  lazy val config: Config                  = ConfigFactory.load()
-  lazy val triedSettings: Try[AppSettings] = AppSettings(config)
 }
---
+++ src/main/scala/com/embedika/ops/Routes.scala
@@ -0,0 +1,42 @@
+package com.embedika.ops
+
+import scala.collection.mutable
+import scala.util.Random
+
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.server.*
+import com.typesafe.scalalogging.LazyLogging
+
+
+/** Provides all routes for a server. Wraps all registered routes with utility directives. */
+trait Routes extends Directives with LazyLogging {
+  private val routesBuffer = new mutable.ArrayBuffer[Route]()
+  private val unhandledExceptionHandler = ExceptionHandler { case e =>
+    logger.error("Unhandled exception occured in routes", e)
+    complete(HttpResponse(StatusCodes.InternalServerError, Nil, s"Unhandled exception: ${e.getMessage}"))
+  }
+
+  def settings: AppSettings
+
+  /** Gets all available routes to serve at a later point. */
+  def routes: Route = mapRequest(setRequestId) {
+    redirectToNoTrailingSlashIfPresent(StatusCodes.MovedPermanently) {
+      handleExceptions(unhandledExceptionHandler) {
+        headerValueByName(settings.requestIdHeaderName) { requestId =>
+          respondWithHeader(RawHeader(settings.requestIdHeaderName, requestId)) {
+            routesBuffer reduce (_ ~ _)
+          }
+        }
+      }
+    }
+  }
+
+  /** Addes a new route to [[routes]]. */
+  def registerRoute(route: Route): Unit = routesBuffer += route
+
+  private def setRequestId(req: HttpRequest): HttpRequest = {
+    val requestId = Random.alphanumeric.take(5).mkString
+    req.addHeader(RawHeader(settings.requestIdHeaderName, requestId))
+  }
+}
